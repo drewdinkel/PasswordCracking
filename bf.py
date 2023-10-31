046d9ac77a139fe24473dd962ea0411d49292853
DrewDinkel <drewdinkel3417@gmail.com>
2023-10-31 10:45:10 -0500

    input questions

--- bf.py
+++ bf.py
@@ -1,5 +1,22 @@
 
+min = input('What is the minimum length of the password? ')
+max = input('What is the maximum length of the password? ')
+chars = input('What characters does the password consist of? ')
+print(min, max, chars)
 
 
-name = input('What is your name?')
-print(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
