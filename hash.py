0886e859c3aef113a97f38e71568616523e635db
DrewDinkel <drewdinkel3417@gmail.com>
2023-11-13 10:57:58 -0600

    md5 working

--- hash.py
+++ hash.py
@@ -1,12 +1,16 @@
 import hashlib
 
-#passList = open("/home/goofygoober/Desktop/PasswordCracking/passList.txt", "r")
-#with open("/home/goofygoober/Desktop/PasswordCracking/passList.txt", "r") as passList:
-#    for line in passList:
-#        print(line)
+password = input("Enter the hash you want to be cracked. ")
+textPath = "/home/goofygoober/Desktop/PasswordCracking/passList.txt"
 
-
-
-my_str = 'password'
-my_hash = hashlib.md5(my_str.encode('utf-8')).hexdigest()
-print(my_hash)
+passList = open(textPath, "r")
+with open(textPath, "r") as passList:
+    for line in passList:
+        #Have to cut last two characters to properly convert hash (Otherwise \n messes up the hash)
+        guess = line.replace('\n', '')
+        #Converts guess into md5 hash (utf-8 is most common encoding / hexdigest() turns into base16)
+        hashGuess = hashlib.md5(guess.encode('utf-8')).hexdigest()
+        print("Trying: " + hashGuess)
+        if (password == hashGuess):
+            print("You've found the password! The md5 hash " + password + " is " + guess + " in plain text.")
+            break
