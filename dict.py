42bcc603d4dba38dc736ca69e40626c18d4e4fb9
DrewDinkel <drewdinkel3417@gmail.com>
2023-11-28 10:35:06 -0600

    Dictionary Working

--- dict.py
+++ dict.py
@@ -1,1 +1,24 @@
-print("This is where dictionary attack will be located")
+textPath = "/home/goofygoober/Desktop/PasswordCracking/passList.txt"
+#Variable to determine if the password was found
+tF = False
+
+def dictAttack():
+    password = input("Enter the password you want to be cracked. ")
+    passList = open(textPath, "r")
+    with open(textPath, "r") as passList:
+        for line in passList:
+            #Cuts out \n so passwords match
+            guess = line.replace("\n", "")
+            print("Trying: " + guess)
+            if (guess == password):
+                #Sets tF to true so line 21 doesn't run
+                tF = True
+                print("You've found the password! The password is " + guess)
+                #Leave function when the password was found
+                break
+        #If password wasn't found print so
+        if (tF == False):
+            print("The password could not be located.")
+
+#Runs the function for testing
+dictAttack()
